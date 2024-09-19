a27a3015ed02cbe88d9939e04ee779d14208d1ec
Jack English <51383165+AUT0M4T1C-JACK@users.noreply.github.com>
2024-09-19 16:02:15 -0400

    in progress, details in description
    - clicking the button to create and add a restaurant to your profile works
    what doesn't work:
    - passing in the name of the restaurant when it's created in the database with the favorite button
    - the button showing "remove from favorites"
    - favorites rendering on your profile view

--- CS2340_Team39/urls.py
+++ CS2340_Team39/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
@@ -21,4 +22,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('restaurants/', include('restaurants.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('admin/', admin.site.urls),
 ]
